15d4ef4dd07b68897d472dbb099cdc61a5d3a4ad
markdid <mark@larsen.org>
2025-06-21 11:33:03 -0400

    init example

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,6 +1,22 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:livekit_components/livekit_components.dart';
+import 'package:logging/logging.dart';
+import 'package:intl/intl.dart';
+import 'package:responsive_builder/responsive_builder.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
+  final format = DateFormat('HH:mm:ss');
+  // configure logs for debugging
+  Logger.root.level = Level.FINE;
+  Logger.root.onRecord.listen((record) {
+    if (kDebugMode) {
+      print('${format.format(record.time)}: ${record.message}');
+    }
+  });
+
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
@@ -10,113 +26,217 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+    return MaterialApp(title: 'Flutter Demo', theme: LiveKitTheme().buildThemeData(context), home: const MyHomePage());
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  //
+  const MyHomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<StatefulWidget> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  static const _storeKeyUri = 'uri';
+  static const _storeKeyToken = 'token';
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  String _url = '';
+  String _token = '';
+
+  void _readPrefs() async {
+    final prefs = await SharedPreferences.getInstance();
+    _url =
+        const bool.hasEnvironment('URL')
+            ? const String.fromEnvironment('URL')
+            : prefs.getString(_storeKeyUri) ?? 'your url here';
+    _token =
+        const bool.hasEnvironment('TOKEN')
+            ? const String.fromEnvironment('TOKEN')
+            : prefs.getString(_storeKeyToken) ?? 'your token here';
+  }
+
+  // Save URL and Token
+  Future<void> _writePrefs(String url, String token) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(_storeKeyUri, url);
+    await prefs.setString(_storeKeyToken, token);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _readPrefs();
+  }
+
+  /// handle join button pressed, fetch connection details and connect to room.
+  // ignore: unused_element
+  void _onJoinPressed(RoomContext roomCtx, String url, String token) async {
+    if (kDebugMode) {
+      print('Joining room: url=$url, token=$token');
+    }
+    await _writePrefs(url, token);
+    try {
+      await roomCtx.connect(url: url, token: token);
+    } catch (e) {
+      if (kDebugMode) {
+        print('Failed to join room: $e');
+      }
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return LivekitRoom(
+      roomContext: RoomContext(
+        enableAudioVisulizer: true,
+        onConnected: () {
+          if (kDebugMode) {
+            print('Connected to room');
+          }
+        },
+        onDisconnected: () {
+          if (kDebugMode) {
+            print('Disconnected from room');
+          }
+        },
+        onError: (error) {
+          if (kDebugMode) {
+            print('Error: $error');
+          }
+        },
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      builder: (context, roomCtx) {
+        var deviceScreenType = getDeviceType(MediaQuery.of(context).size);
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text('LiveKit Components', style: TextStyle(color: Colors.white)),
+            actions: [
+              /// show clear pin button
+              if (roomCtx.connected) const ClearPinButton(),
+            ],
+          ),
+          body: Stack(
+            children: [
+              !roomCtx.connected && !roomCtx.connecting
+                  /// show prejoin screen if not connected
+                  ? Prejoin(token: _token, url: _url, onJoinPressed: _onJoinPressed)
+                  :
+                  /// show room screen if connected
+                  Row(
+                    children: [
+                      /// show chat widget on mobile
+                      (deviceScreenType == DeviceScreenType.mobile && roomCtx.isChatEnabled)
+                          ? Expanded(
+                            child: ChatBuilder(
+                              builder: (context, enabled, chatCtx, messages) {
+                                return ChatWidget(
+                                  messages: messages,
+                                  onSend: (message) => chatCtx.sendMessage(message),
+                                  onClose: () {
+                                    chatCtx.toggleChat(false);
+                                  },
+                                );
+                              },
+                            ),
+                          )
+                          : Expanded(
+                            flex: 6,
+                            child: Stack(
+                              children: <Widget>[
+                                /* Expanded(
+                                      child: TranscriptionBuilder(
+                                        builder:
+                                            (context, roomCtx, transcriptions) {
+                                          return TranscriptionWidget(
+                                            transcriptions: transcriptions,
+                                          );
+                                        },
+                                      ),
+                                    ),*/
+                                /// show participant loop
+                                ParticipantLoop(
+                                  showAudioTracks: true,
+                                  showVideoTracks: true,
+                                  showParticipantPlaceholder: true,
+
+                                  /// layout builder
+                                  layoutBuilder:
+                                      roomCtx.pinnedTracks.isNotEmpty
+                                          ? const CarouselLayoutBuilder()
+                                          : const GridLayoutBuilder(),
+
+                                  /// participant builder
+                                  participantTrackBuilder: (context, identifier) {
+                                    // build participant widget for each Track
+                                    return Padding(
+                                      padding: const EdgeInsets.all(2.0),
+                                      child: Stack(
+                                        children: [
+                                          /// video track widget in the background
+                                          identifier.isAudio && roomCtx.enableAudioVisulizer
+                                              ? const AudioVisualizerWidget(backgroundColor: LKColors.lkDarkBlue)
+                                              : IsSpeakingIndicator(
+                                                builder: (context, isSpeaking) {
+                                                  return isSpeaking != null
+                                                      ? IsSpeakingIndicatorWidget(
+                                                        isSpeaking: isSpeaking,
+                                                        child: const VideoTrackWidget(),
+                                                      )
+                                                      : const VideoTrackWidget();
+                                                },
+                                              ),
+
+                                          /// focus toggle button at the top right
+                                          const Positioned(top: 0, right: 0, child: FocusToggle()),
+
+                                          /// track stats at the top left
+                                          const Positioned(top: 8, left: 0, child: TrackStatsWidget()),
+
+                                          /// status bar at the bottom
+                                          const Positioned(bottom: 0, left: 0, right: 0, child: ParticipantStatusBar()),
+                                        ],
+                                      ),
+                                    );
+                                  },
+                                ),
+
+                                /// show control bar at the bottom
+                                const Positioned(bottom: 30, left: 0, right: 0, child: ControlBar()),
+                              ],
+                            ),
+                          ),
+
+                      /// show chat widget on desktop
+                      (deviceScreenType != DeviceScreenType.mobile && roomCtx.isChatEnabled)
+                          ? Expanded(
+                            flex: 2,
+                            child: SizedBox(
+                              width: 400,
+                              child: ChatBuilder(
+                                builder: (context, enabled, chatCtx, messages) {
+                                  return ChatWidget(
+                                    messages: messages,
+                                    onSend: (message) => chatCtx.sendMessage(message),
+                                    onClose: () {
+                                      chatCtx.toggleChat(false);
+                                    },
+                                  );
+                                },
+                              ),
+                            ),
+                          )
+                          : const SizedBox(width: 0, height: 0),
+                    ],
+                  ),
+
+              /// show toast widget
+              const Positioned(top: 30, left: 0, right: 0, child: ToastWidget()),
+            ],
+          ),
+        );
+      },
     );
   }
 }
